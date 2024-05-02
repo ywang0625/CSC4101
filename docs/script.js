74ff43fd1bbeb63aacacca0bd201e18e6d4062e8
ywang0625 <yuehming.wang0625@gmail.com>
2024-05-02 15:15:33 -0500

    Improvement

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,6 @@
 //Fetching the parser table
 function get_cell(last_stack, first_input) {
+
     if(!(first_input) || !(last_stack)){
         //document.getElementById("error-msg").innerHTML="Error: Fetch expression failure.\n";
         throw new Error("Error: Fetch input failure\n");
@@ -7,31 +8,50 @@ function get_cell(last_stack, first_input) {
 
     const col = get_column(first_input)[0];
     const row = get_row(last_stack.join(""));
+    const parser_table = document.getElementById('parser-table');
+    const target_row = parser_table.rows[row + 2];
+    const target_cell = target_row.cells[col];
 
-    document.getElementById('parser-table').rows[row + 2].cells[col].style.backgroundColor = 'yellow';
-
-
-    //Checking to see if anything existed in session storage
-    if (sessionStorage.getItem("oldrow")) {
-        oldrow = JSON.parse(sessionStorage.getItem("oldrow"));
+    // //lastminfix
+    if (!parser_table) {
+        throw new Error("Table not found");
+    }
+    if (!target_row) {
+        throw new Error("Target row not found");
+    }
+    if (!target_cell) {
+        throw new Error("Target cell not found");
     }
 
-    if (sessionStorage.getItem("oldcol")) {
-        oldcol = JSON.parse(sessionStorage.getItem("oldcol"));
-    }
-
-    if (sessionStorage.getItem("oldrow") && sessionStorage.getItem("oldcol")) {
-
-        document.getElementById('parser-table').rows[parseInt(oldrow) + 2].cells[parseInt(oldcol)].style.backgroundColor = '';
-
-    }
-
-    sessionStorage.setItem("oldrow", JSON.stringify(row.toString()));
-    sessionStorage.setItem("oldcol", JSON.stringify(col.toString()));
+    target_cell.style.backgroundColor = '#DBADEE';
+    clear_prev_highlight();
 
 
-    return document.getElementById('parser-table').rows[row + 2].cells[col].textContent;
+    // Storing current cell's position
+    sessionStorage.setItem("oldrow", JSON.stringify(row));
+    sessionStorage.setItem("oldcol", JSON.stringify(col));
+
+    //lastminfix
+    return target_cell.textContent;
+
 }
+
+//lastminfix
+function clear_prev_highlight() {
+    if (sessionStorage.getItem("oldrow") && sessionStorage.getItem("oldcol")) {
+        //fetch data
+        const oldrow = JSON.parse(sessionStorage.getItem("oldrow"));
+        const oldcol = JSON.parse(sessionStorage.getItem("oldcol"));
+
+        //get the cell
+        const oldCell = document.getElementById('parser-table').rows[oldrow + 2].cells[oldcol];
+       
+        oldCell.style.backgroundColor = '';
+    }
+}
+
+
+
 
 function get_column(first_input){
     console.log("\nGetting column... ");
@@ -73,14 +93,16 @@ function get_column(first_input){
 }
 
 function get_row(last_stack){
+    let row_test;
     console.log("\n Getting row...");
 
     if (!last_stack){
         throw new Error("Error: last_stack not found");
     }
+
     switch(last_stack.slice(-2)){
         case "10":
-            return 10; 
+            return 10;
         case "11":
             return 11; 
     }
@@ -98,18 +120,14 @@ function get_row(last_stack){
     }
 }
 
-
-
-
-
 //Translating 
 function parse(last_stack, first_input) {
     const element = get_cell(last_stack, first_input);
-    console.log("The element: " + element);
+    let arrToAdd;
+
 
     if (element.substring(0, 1) === "S") {
         console.log("Shifting");
-        let arrToAdd;
         [arrToAdd, first_input] = shift(last_stack, first_input, element.substring(1, element.length));
         last_stack = last_stack.concat(arrToAdd);
     } 
@@ -129,15 +147,17 @@ function parse(last_stack, first_input) {
 //Shift function
 function shift(last_stack, first_input, number) {
     last_stack = [get_column(first_input)[1], number];
+    //remove the one's being shifted 
     first_input = first_input.substring(get_column(first_input)[1].length);
 
+    //return updated value
     return [last_stack, first_input];
 }
 
 
 //Reduced function
 function reduce(last_stack, first_input, number) {
-    let add_in, replaced;
+    let add_in, replaced, element;
 
     //reduce grammar rule 
         switch (number) {
@@ -161,7 +181,6 @@ function reduce(last_stack, first_input, number) {
                 break;
         }
     
-
     if (replaced === "") {
         last_stack[last_stack.length - 2] = add_in;
     } 
@@ -172,10 +191,16 @@ function reduce(last_stack, first_input, number) {
             console.log("at position: " + last_stack[i-1]);
             if(last_stack[i-1] === replaced){
                 console.log("Target found");
-                last_stack = last_stack.slice(0, i-2); 
-                last_stack[last_stack.length - 1] = "F"; 
+                last_stack = last_stack.slice(0, i); 
+                last_stack[last_stack.length - 1] = "F";
+                console.log(last_stack[i-1]);
             }
-        }           
+        }  
+        temp = last_stack[last_stack.length - 2];  
+        if (temp === "0" || temp === "4" || temp === "4")
+            last_stack[last_stack.length - 1] = "3";
+        else if (temp === "7")
+            last_stack[last_stack.length - 1] = "10";
     }
     else
     {
@@ -192,68 +217,65 @@ function reduce(last_stack, first_input, number) {
             }
         }
     }
-
-    if (last_stack[last_stack.length - 1] === "F"){
-        last_stack[last_stack.length - 1] = document.getElementById("parser-table").rows[parseInt(last_stack[last_stack.length - 3]) + 2].cells[9].textContent;
-    }
     
     switch (last_stack[last_stack.length - 2]) {
         case "E":
-            console.log("E!");
-            console.log(parseInt(parseInt(last_stack[last_stack.length - 3])) + 2);
             last_stack[last_stack.length - 1] = document.getElementById("parser-table").rows[parseInt(last_stack[last_stack.length - 3]) + 2].cells[7].textContent;
             break;
         case "T":
-            console.log("T!");
-            console.log(parseInt(parseInt(last_stack[last_stack.length - 3])) + 2);
             last_stack[last_stack.length - 1] = document.getElementById("parser-table").rows[parseInt(last_stack[last_stack.length - 3]) + 2].cells[8].textContent;
             break;
         case "F":
-            console.log("F!");
             last_stack[last_stack.length - 1] = document.getElementById("parser-table").rows[parseInt(last_stack[last_stack.length - 3]) + 2].cells[9].textContent;
             break;
     }
 
+
     return last_stack;
 }
-
 
 //The exectuions
 //When clicking the execution button
 function execution() {
-
-
     console.log("\n\n Executing next execution... ");
-    const output = document.getElementById("output");
-    var parsed_array = [];
+    let output = document.getElementById("output");
+    var parsed_array = [], expression;
 
+    //For non-existence input
     if(!(document.getElementById("expression").value)){
         window.onerror = function() {
             alert("Please enter an expression!");
+            location.reload();
             return true;}
     }
 
-    if (sessionStorage.getItem("parsed_array")) {
+    //retrive values if they exist
+    if (sessionStorage.length > 0){
         parsed_array = JSON.parse(sessionStorage.getItem("parsed_array"));
     }
 
-    if (output.rows.length === 0) {
+    if (!(output.rows.length > 0)) {
+        //the output is non existence (initializing...)
         push_item(output, "0", document.getElementById("expression").value)
         parsed_array = [0];
     } 
     else 
     {
-        var first_input = output.rows[output.rows.length - 1].cells[1].innerHTML;
-        [parsed_array, first_input] = parse(parsed_array, first_input);
+        //data exists in the sessionStorage -> retrive them back
+        //set the expression as the last existing data from the column 1 table
+        expression = output.rows[output.rows.length - 1].cells[1].innerHTML;
+        
+        //continue to parse 
+        [parsed_array, expression] = parse(parsed_array, expression);
 
-        //if (parsed_array.join("") === "0E1" && first_input === "$") {
-        if (first_input === "$" && parsed_array.join("") === "0E1"){
-
-            oldrow = JSON.parse(sessionStorage.getItem("oldrow"));
-            oldcol = JSON.parse(sessionStorage.getItem("oldcol"));
+        //if we reach the end (aka accept)
+        if (expression === "$" && parsed_array.join("") === "0E1"){
 
             //clear the previous one
-            document.getElementById("parser-table").rows[parseInt(oldrow) + 2].cells[parseInt(oldcol)].style.backgroundColor = '';
+            //document.getElementById("parser-table").rows[parseInt(JSON.parse(sessionStorage.getItem("oldrow"))) + 2].cells[parseInt(JSON.parse(sessionStorage.getItem("oldcol")))].style.backgroundColor = '';
+            clear_prev_highlight();
+
+
             //change color
             document.getElementById("parser-table").rows[3].cells[6].style.backgroundColor = '#9bfc92';
             document.getElementById("parser-table").rows[3].cells[6].style.color = "#5A5B60";
@@ -263,15 +285,14 @@ function execution() {
             document.getElementById("execution").disabled = true;
         }
 
-        push_item(output, parsed_array.join(""), first_input)
+        push_item(output, parsed_array.join(""), expression)
 
     }
-
-    //store parsed array in storage
+    //store parsed array in storage as string key-value pairs
     sessionStorage.setItem("parsed_array", JSON.stringify(parsed_array));
 }
 
-//To show the output executions
+//To show the output executions 
 function push_item(output_table, column1_data, column2_data) {
     let row = output_table.insertRow(-1);
     let col1 = row.insertCell(0);
